f1056a74b605422048912f1531263dedf0dda1b3
Mildredsambo <57663454+Mildredsambo@users.noreply.github.com>
2019-11-14 12:02:36 +0200

    Add files via upload

---
+++ javascript/robotChap7.js
@@ -0,0 +1,80 @@
+// Put yellow dots around his right eye.
+document.getElementById("righteye").style.border =
+    "4px yellow dotted";
+
+// Change his left arm’s color.
+document.getElementById("leftarm").style.backgroundColor =
+    "#FF00FF";
+
+// Change the text color.
+document.getElementById("body").style.color = "#FF0000";
+
+// Give Douglas hair.
+document.getElementById("head").style.borderTop =
+    "5px black solid";
+
+//document.getElementById("head").style.transform = "rotate(15deg)";
+
+var rightEye = document.getElementById("righteye");
+rightEye.addEventListener("click", moveUpDown);
+
+function moveUpDown(e) {
+    var robotPart = e.target;
+    var top = 0;
+    var id = setInterval(frame, 10) // draw every 10ms
+    function frame() {
+        robotPart.style.top = top + '%';
+        top++;
+        if (top === 20) {
+            clearInterval(id);
+        }
+    }
+}
+
+var nose = document.getElementById("nose");
+nose.addEventListener("click", moveNoseRight);
+
+function moveNoseRight(e) {
+    var robotPart = e.target;
+    var left = 100;
+    var id = setInterval(frame, 10) // draw every 10ms
+    function frame() {
+        robotPart.style.left = left + '%';
+        left--;
+        if (left === 50) {
+            clearInterval(id);
+        }
+    }
+}
+
+var mouth = document.getElementById("mouth");
+mouth.addEventListener("click", movemouthRight);
+
+function movemouthRight(e) {
+    var robotPart = e.target;
+    var left = 100;
+    var id = setInterval(frame, 10) // draw every 10ms
+    function frame() {
+        robotPart.style.left = left + '%';
+        left--;
+        if (left === 20) {
+            clearInterval(id);
+        }
+    }
+}
+
+var leftarm = document.getElementById("leftarm");
+leftarm.addEventListener("click", moveArmDown);
+
+function moveArmDown(e) {
+    var robotPart = e.target;
+    var top = 100;
+    var id = setInterval(frame, 10) // draw every 10ms
+    function frame() {
+        robotPart.style.top = top + '%';
+        top--;
+        if (top === 34) {
+            clearInterval(id);
+        }
+    }
+}
